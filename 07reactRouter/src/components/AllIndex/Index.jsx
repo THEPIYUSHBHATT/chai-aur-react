49a47652202f46b2ea343213313e7977eb161501
piyush <piyushbhatt053@gmail.com>
2024-03-02 12:31:11 +0530

    changes'

---
+++ 07reactRouter/src/components/AllIndex/Index.jsx
@@ -0,0 +1,6 @@
+import About from "../About/About";
+import Contact from "../Contact/Contact";
+import Home from "../Home/Home";
+export default About;
+export default Contact;
+export default Home;
